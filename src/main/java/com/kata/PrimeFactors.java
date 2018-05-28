7bc2b71e37518d7517adc7385dfeeb9311e5caf3
Lionel Durand <lionel2.durand@orange.com>
2018-05-28 17:26:48 +0200

    FactorsOf(2) : green test

--- src/main/java/com/kata/PrimeFactors.java
+++ src/main/java/com/kata/PrimeFactors.java
@@ -2,10 +2,15 @@ package com.kata;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 
 public class PrimeFactors {
     public List<Integer> factorsOf(Integer n) {
+
+        if (n  == 2) {
+            return asList(2);
+        }
         return emptyList();
     }
 }
