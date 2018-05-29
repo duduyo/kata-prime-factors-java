b0ac8a39d004659c8fd20690c5f3993773b0083f
Lionel Durand <lionel2.durand@orange.com>
2018-05-29 21:32:05 +0200

    FactorsOf(8) : refacto part 1

--- src/main/java/com/kata/PrimeFactors.java
+++ src/main/java/com/kata/PrimeFactors.java
@@ -7,19 +7,19 @@ public class PrimeFactors {
     public List<Integer> factorsOf(Integer n) {
 
         List<Integer> integers = new ArrayList<>();
-        if (n  == 2) {
+        if (n == 8) {
             integers.add(2);
-        }
-        if (n  == 4) {
-            integers.add(2);
-            integers.add(2);
-        }
-        if (n  == 8) {
-            integers.add(2);
-            integers.add(2);
-            integers.add(2);
+            n /= 2;
         }
 
+        if (n == 4) {
+            integers.add(2);
+            n /= 2;
+        }
+
+        if (n == 2) {
+            integers.add(2);
+        }
         return integers;
     }
 }
