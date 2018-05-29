2f502757a14e96c9c595ab6cdb5bc5e67e0b7983
Lionel Durand <lionel2.durand@orange.com>
2018-05-29 21:51:26 +0200

    FactorsOf(5/25/125), FactorsOf(30), FactorsOf(BigNumber)

--- src/test/java/com/kata/PrimeFactorsTest.java
+++ src/test/java/com/kata/PrimeFactorsTest.java
@@ -33,4 +33,22 @@ public class PrimeFactorsTest {
         assertThat(primeFactors.factorsOf(3*3*3)).containsExactlyInAnyOrder(3, 3, 3);
     }
 
+    @Test
+    void should_return_factors_for_power_of_5() {
+        assertThat(primeFactors.factorsOf(5)).containsExactlyInAnyOrder(5);
+        assertThat(primeFactors.factorsOf(5*5)).containsExactlyInAnyOrder(5, 5);
+        assertThat(primeFactors.factorsOf(5*5*5)).containsExactlyInAnyOrder(5, 5, 5);
+    }
+
+
+    @Test
+    void should_return_factors_for_30() {
+        assertThat(primeFactors.factorsOf(30)).containsExactlyInAnyOrder(2, 3, 5);
+    }
+
+
+    @Test
+    void should_return_factors_for_big_number() {
+        assertThat(primeFactors.factorsOf(43961)).containsExactlyInAnyOrder(43961);
+    }
 }
